ba1a8bac4817da637c677ab482974faea9a2941b
fodedoucara <fodedoucara2003@gmail.com>
2025-11-21 16:07:08 -0600

    replace default App component with Home component and add Home page

---
+++ ohmworks-frontend/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home(){
+    return(
+        <div> 
+            <h1> Ohmworks - The path to least resistance</h1>
+            <p> Design, test, and simulate electronics - all in your browser</p>
+        </div>
+    )
+}
