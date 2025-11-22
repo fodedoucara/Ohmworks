5028925d5faeddfd5d8d15ec5fffca92b49a81bd
fodedoucara <fodedoucara2003@gmail.com>
2025-11-22 11:04:59 -0600

    create COMPONENT_LIBRARY with initial components

---
+++ ohmworks-frontend/src/data/electronicComponents.js
@@ -0,0 +1,6 @@
+export const COMPONENT_LIBRARY = [
+  { id: "resistor", name: "Resistor", defaultProps: { resistance: 1000 } },
+  { id: "led", name: "LED", defaultProps: { color: "red" } },
+  { id: "sensor", name: "Sensor", defaultProps: {} },
+  { id: "microcontroller", name: "Microcontroller", defaultProps: {} },
+];
